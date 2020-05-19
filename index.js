df8a088684175e79fb0afa0cc5e543a2a9525254
kyle <macadmin@MacVicdeMacBook-Pro.local>
2020-05-19 11:45:22 +0800

    1 Week of Progress

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelector(".one").addEventListener("click", function(){
+  window.location = "about.html";
+});
+
+document.querySelector(".two").addEventListener("click", function(){
+  window.location = "link";
+});
+
+document.querySelector(".three").addEventListener("click", function(){
+  window.location = "link";
+});
+
+document.querySelector(".four").addEventListener("click", function(){
+  window.location = "link";
+});
+
+document.querySelector(".five").addEventListener("click", function(){
+  window.location = "link";
+});
