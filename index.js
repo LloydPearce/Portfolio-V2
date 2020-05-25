6b90220a66863d9b4d37ac63d099b2f6366be107
kyle <macadmin@MacVicdeMacBook-Pro.local>
2020-05-25 17:38:42 +0800

    Implementation of Responsiveness

--- index.js
+++ index.js
@@ -26,11 +26,20 @@ function goHome(){
   window.location = "index.html";
 }
 
+const navSlide = () =>{
+  const smallNav = document.querySelector(".smallNav");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  smallNav.addEventListener("click", ()=>{
+    nav.classList.toggle("nav-active");
+  });
+
+  navLinks.forEach((link, index)=>{
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+2}s`;
+  });
+}
+
+navSlide();
 
 $(".main-content").delay(800).animate({"opacity": "1"}, 700);
-
-$(".who-section").delay(800).animate({"opacity": "1"}, 700);
-
-$(".main").delay(800).animate({"opacity": "1"}, 700);
-
-$(".skills-information").delay(800).animate({"opacity": "1"}, 700);
